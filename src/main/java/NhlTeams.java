78ecbe7b44c91b7e097d70fb28ec2f4c1daf86d7
Zdenek Vilim <zdenek.vilim@classedu.com>
2023-11-07 08:30:26 +0100

    first commit

--- src/main/java/NhlTeams.java
+++ src/main/java/NhlTeams.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 public class NhlTeams {
 
     private static final ApiReader apiReader = new ApiReader();
